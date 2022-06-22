0b0fef542572a3a6205bb20717919e4a0316f2f6
Vadim Chupin <chup_98@mail.ru>
2022-06-22 19:35:19 +0500

    Improve code
    Create method for simplification the code. Change concatenation of strings from via operator '+' to special function 'concat'.

--- src/Main.java
+++ src/Main.java
@@ -3,43 +3,52 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         while (true) {
-            System.out.println("Введите натуральное число");
+            System.out.println("Enter a positive integer number...");
 
             Scanner scan = new Scanner (System.in);
             int number = scan.nextInt();
 
-            String rome = "";
-            String[][] numerals = {{"I", "X", "C", "M"}, {"V", "L", "D"}};
-
-            if (number / 1000 >= 1) {
-                rome = numerals[0][3].repeat(number / 1000);
-                number = number % 1000;
+            if (number <= 0) {
+                System.out.println("Error - enter a positive integer number!\n");
+            } else {
+                String rome = arabicToRome(number);
+                System.out.println(rome + "\n");
             }
+        }
+    }
 
-            for (int i = 2; i >= 0; i--) {
-                if (number / ((int) Math.pow(10, i)) == 9) {
-                    rome += numerals[0][i] + numerals[0][i + 1];
-                    number = number % (9 * ((int) Math.pow(10, i)));
+    private static String arabicToRome(int number) {
+        String rome = "";
+        String[][] numerals = {{"I", "X", "C", "M"}, {"V", "L", "D"}};
+
+        if (number / 1000 >= 1) {
+            rome = numerals[0][3].repeat(number / 1000);
+            number = number % 1000;
+        }
+
+        for (int i = 2; i >= 0; i--) {
+            if (number / ((int) Math.pow(10, i)) == 9) {
+                rome = rome.concat(numerals[0][i]).concat(numerals[0][i + 1]);
+                number = number % (9 * ((int) Math.pow(10, i)));
+            }
+            else {
+                if (number / ((int) Math.pow(10, i)) == 4) {
+                    rome = rome.concat(numerals[0][i]).concat(numerals[1][i]);
+                    number = number % (4 * ((int) Math.pow(10, i)));
                 }
                 else {
-                    if (number / ((int) Math.pow(10, i)) == 4) {
-                        rome += numerals[0][i] + numerals[1][i];
-                        number = number % (4 * ((int) Math.pow(10, i)));
+                    if (number / (5 * ((int) Math.pow(10, i))) >= 1) {
+                        rome = rome.concat(numerals[1][i]);
+                        number = number % (5 * ((int) Math.pow(10, i)));
                     }
-                    else {
-                        if (number / (5 * ((int) Math.pow(10, i))) >= 1) {
-                            rome += numerals[1][i];
-                            number = number % (5 * ((int) Math.pow(10, i)));
-                        }
-                        if (number / ((int) Math.pow(10, i)) >= 1) {
-                            rome += numerals[0][i].repeat(number / ((int) Math.pow(10, i)));
-                            number = number % (((int) Math.pow(10, i)));
-                        }
+                    if (number / ((int) Math.pow(10, i)) >= 1) {
+                        rome = rome.concat(numerals[0][i].repeat(number / ((int) Math.pow(10, i))));
+                        number = number % (((int) Math.pow(10, i)));
                     }
                 }
             }
-
-            System.out.println(rome);
         }
+
+        return rome;
     }
 }
